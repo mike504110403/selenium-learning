8638e5bb4eed6829553ecd2fe07b0dc72a9a9cac
Mike Lin <mike504110403@gmail.com>
2021-07-30 20:37:31 +0800

    first commit
    start of web automation test - web pages reptile

---
+++ puthon_selenium_pj1/learning_selenium/FirstAutomationTest.py
@@ -0,0 +1,55 @@
+from selenium import webdriver  # webdriver package
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager  # drivermanager package
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+# 使用.wdm指定driver位置
+driver = webdriver.Chrome(ChromeDriverManager().install())  # 將webdriver設至變數driver
+
+# driver = webdriver.Chrome(executable_path="C:\\browser_drivers\\chromedriver.exe")
+
+driver.maximize_window()  # 最大化視窗
+driver.get("https://www.dcard.tw/f")  # 開啟指定url
+
+#於搜尋欄輸入文字
+search = driver.find_element_by_name("query")
+search.clear()  # 清空輸入欄位
+search.send_keys("西斯")  # 輸入文字
+search.send_keys(Keys.RETURN)  # enter
+
+# 等待看板顯示後再開始取title
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "sc-3yr054-1"))
+)
+
+# 將標題放入titles
+titles = driver.find_elements_by_class_name("tgn9uw-3")
+
+# 寫出標題
+for title in titles:
+    print(title.text)
+
+time.sleep(5)
+
+# 點擊特定標籤
+link = driver.find_element_by_link_text("被征服的任性女友")
+link.click()
+
+driver.back()
+driver.back()
+driver.forward()
+
+time.sleep(5)
+driver.close()  # 關閉視窗
+
+
+# print(driver.title)
+# driver.get("http://www.google.com")
+# print(driver.title)
+# driver.back()
+# print(driver.title)
+# driver.forward()
+# print(driver.title)
